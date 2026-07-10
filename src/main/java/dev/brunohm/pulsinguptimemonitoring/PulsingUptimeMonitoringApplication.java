2260ef3c0f6f3ac5b754c3f509b676426999a081
brunohmdev <brunohm.dev@gmail.com>
2026-07-10 16:21:05 -0300

    first commit

---
+++ src/main/java/dev/brunohm/pulsinguptimemonitoring/PulsingUptimeMonitoringApplication.java
@@ -0,0 +1,13 @@
+package dev.brunohm.pulsinguptimemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsingUptimeMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulsingUptimeMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/brunohm/pulsinguptimemonitoring/PulsingUptimeMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brunohm.pulsinguptimemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsingUptimeMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
